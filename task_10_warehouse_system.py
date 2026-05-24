e53038e94319b6a43acdfa93d182b088090a3142
ConstantineWorkspace <volokhin.space@gmail,com>
2026-05-24 20:07:05 +0300

    task10: рассчет общей стоимости

--- task_10_warehouse_system.py
+++ task_10_warehouse_system.py
@@ -21,3 +21,12 @@ for name, data in warehouse.items():
     total = data["quantity"] * data["price"]
     critical = " ⚠ КРИТИЧ!" if data["quantity"] < data["min_quantity"] else ""
     print(f"{name:<12} | {data['quantity']:>6} | {data['price']:>8.2f} | {data['min_quantity']:>5} | {total:.2f}{critical}")
+    # --- Общая стоимость ---
+total_cost = sum(d["quantity"] * d["price"] for d in warehouse.values())
+print("=" * 78)
+print(f"ОБЩАЯ СТОИМОСТЬ: {total_cost:.2f} руб")
+
+# --- Самый дорогой материал ---
+most_expensive = max(warehouse, key=lambda x: warehouse[x]["quantity"] * warehouse[x]["price"])
+most_exp_cost = warehouse[most_expensive]["quantity"] * warehouse[most_expensive]["price"]
+print(f"Самый дорогой: {most_expensive} ({most_exp_cost:.2f} руб)")
